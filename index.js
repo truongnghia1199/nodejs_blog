12e4741172fada6a8ecea52dd98aa99ffa38d513
nghia.tt <nghia.tt@ptf.com.vn>
2022-05-16 11:18:08 +0700

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3001
 
+app.use(morgan('combined'))
 app.get('/', (req, res) => {
   var a = 1;
 var b = 2;
